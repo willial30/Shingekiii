47418dbefa1d94a79fae90ca70112cac885db70d
willial30 <rivera.william914@gmail.com>
2025-02-01 00:15:58 -0500

    Agregar archivos de proyecto.

---
+++ Shingeki/Pages/Levi.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shingeki.Pages
+{
+    public class LeviModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
